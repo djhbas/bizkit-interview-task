c6bc1cf92cfc7d52aeac34f89dcb1e7ce555d3c6
David John Bas <95739092+djhbas@users.noreply.github.com>
2024-07-02 17:50:28 +0800

    Update match.py

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,9 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
+    fave_numbers_set = set(fave_numbers_1)
     for number in fave_numbers_2:
-        if number not in fave_numbers_1:
+        if number not in fave_numbers_set:
             return False
 
     return True
